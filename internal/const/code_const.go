3555781c9ce786ab11bea49dc33795c400bcea21
phananhtu1998 <109066304+phananhtu1998@users.noreply.github.com>
2025-04-01 09:44:20 +0700

    Update code_const.go

--- internal/const/code_const.go
+++ internal/const/code_const.go
@@ -8,8 +8,8 @@ const (
 	REFRESH_TOKEN             int    = 168
 	HOST_EMAIL                string = "phananhtu1998@gmail.com"
 	HOST_PREFIX               string = "/v1/2025"
-	RATELIMIT_REQUEST_PRIVATE int    = 5
-	RATELIMIT_SECOND__PRIVATE int    = 10
+	RATELIMIT_REQUEST_PRIVATE int    = 50
+	RATELIMIT_SECOND__PRIVATE int    = 1
 	RATELIMIT_REQUEST_GLOBAL  int    = 1000
 	RATELIMIT_SECOND__GLOBAL  int    = 1
 )
